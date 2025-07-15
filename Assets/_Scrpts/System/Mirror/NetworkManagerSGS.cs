db36c2c64e6381c4c9e852e59ef1fdea316ec059
MinhCT6 <minhct6@fpt.com>
2025-07-15 10:07:58 +0700

    setup mirror

---
+++ Assets/_Scrpts/System/Mirror/NetworkManagerSGS.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+
+// Doesnt do anything special but it's set up to be built-upon
+[AddComponentMenu("Network Manager CCG")]
+public class NetworkManagerSGS : NetworkManager
+{
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        Transform startPos = GetStartPosition();
+        GameObject player = Instantiate(playerPrefab);
+
+        NetworkServer.AddPlayerForConnection(conn, player);
+    }
+}
