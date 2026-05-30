03d77a1d5a6543475fa6691c28c2ec4960a29489
Katt1603 <daydreamcat1603@gmail.com>
2026-05-30 22:11:53 +0800

    fixing ui for tool tips

--- Assets/_Scrpts/Extensions/ToolTip.cs
+++ Assets/_Scrpts/Extensions/ToolTip.cs
@@ -1,6 +1,7 @@
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Xml.Serialization;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,17 +11,14 @@ public class ToolTip : Singleton<ToolTip>
 {
     [SerializeField]
     private Camera uiCamera;
-    private Text toolTipText;
-    private Text description;
-    private RectTransform backgroundRectTransform;
-    [SerializeField] private GameObject toolText;
+    private TextMeshProUGUI toolTipText;
+    private TextMeshProUGUI description;
     [SerializeField] private GameObject background;
     protected override void Awake()
     {
         base.Awake();
-        backgroundRectTransform = transform.Find("BackGround").GetComponent<RectTransform>();
-        toolTipText = transform.Find("Name").GetComponent<Text>();
-        description = transform.Find("Description").GetComponent<Text>();
+        toolTipText = transform.Find("Name").GetComponent<TextMeshProUGUI>();
+        description = transform.Find("Description").GetComponent<TextMeshProUGUI>();
         HideToolTip();
     }
     public void Update()
@@ -34,10 +32,7 @@ public class ToolTip : Singleton<ToolTip>
         gameObject.SetActive(true);
         toolTipText.text = toolTipString;
         description.text = toolTipDescription;
-        float textPaddingSize = 4f;
-        UnityEngine.Vector2 backgroundSize = new UnityEngine.Vector2(300f + textPaddingSize * 2f, 150f + textPaddingSize * 2f);
-        backgroundRectTransform.sizeDelta = backgroundSize;
-        UnityEngine.Vector2 descriptionSize = new UnityEngine.Vector2(300f + textPaddingSize * 2f, description.preferredHeight - textPaddingSize * 2f);
+        UnityEngine.Vector2 descriptionSize = new UnityEngine.Vector2(300f, description.preferredHeight);
         description.rectTransform.sizeDelta = descriptionSize;
     }
     public void HideToolTip()
